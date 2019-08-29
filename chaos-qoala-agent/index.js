487e7869c20286d761e14fe5024b4d8b2ed45be2
Jake Ory <jacobtory@yahoo.com>
2019-08-28 23:24:39 -0700

    stop functionality and results writing to file

--- chaos-qoala-agent/index.js
+++ chaos-qoala-agent/index.js
@@ -5,7 +5,7 @@
 /* eslint-disable default-case */
 // 99% of the code below was taken from this handy npm module
 // https://github.com/zuojiang/modify-response-middleware/tree/master/src
-const zlib = require('zlib');
+const zlib = require("zlib");
 
 let DELAY_RESPONSE_MS = 0;
 let REQUESTS_SO_FAR = 0;
@@ -13,15 +13,14 @@ let QUERY_TARGETS;
 let BLAST_RADIUS;
 let CHAOS_CHANCE;
 let ENSUE_CHAOS = false;
-const app = require('express')();
-const chaosSocketServer = require('http').Server(app);
-const io = require('socket.io')(chaosSocketServer);
+const app = require("express")();
+const chaosSocketServer = require("http").Server(app);
+const io = require("socket.io")(chaosSocketServer);
 
 chaosSocketServer.listen(1025);
 
-io.on('connection', (socket) => {
-  socket.on('eucalyptus', (config, acknowledge) => {
-    console.log(config);
+io.on("connection", socket => {
+  socket.on("eucalyptus", (config, acknowledge) => {
     ENSUE_CHAOS = config.ensueChaos;
     const { delay } = config;
     const { blastRadius } = config;
@@ -33,40 +32,34 @@ io.on('connection', (socket) => {
   });
 });
 
-io.on('close', (socket) => {
-  socket.on('client-msg', (config, fn) => {
-  });
-});
-
-
 function encoding(res, content) {
-  switch (res.getHeader('content-encoding')) {
-    case 'gzip':
+  switch (res.getHeader("content-encoding")) {
+    case "gzip":
       return zlib.gzipSync(content);
-    case 'deflate':
+    case "deflate":
       return zlib.deflateSync(content);
   }
   return content;
 }
 
 function decoding(res, content) {
-  switch (res.getHeader('content-encoding')) {
-    case 'gzip':
+  switch (res.getHeader("content-encoding")) {
+    case "gzip":
       return zlib.gunzipSync(content);
-    case 'deflate':
+    case "deflate":
       return zlib.inflateSync(content);
   }
   return content;
 }
 
-const modifyRes = function (modify) {
+const modifyRes = function(modify) {
   return (req, res, next) => {
     const _end = res.end;
     const list = [];
-    res.write = function (chunk) {
+    res.write = function(chunk) {
       list.push(chunk);
     };
-    res.end = function (chunk) {
+    res.end = function(chunk) {
       if (chunk) {
         list.push(chunk);
       }
@@ -74,49 +67,53 @@ const modifyRes = function (modify) {
       if (Buffer.isBuffer(list[0])) {
         content = Buffer.concat(list);
       } else {
-        content = list.join('');
+        content = list.join("");
       }
       let _content = decoding(res, content);
       _content = modify(_content, req, res);
-      if (Buffer.isBuffer(_content) || typeof _content === 'string') {
+      if (Buffer.isBuffer(_content) || typeof _content === "string") {
         content = encoding(res, _content);
         if (!res.headersSent) {
-          res.setHeader('content-length', content.length);
+          res.setHeader("content-length", content.length);
         }
       }
-      if (ENSUE_CHAOS === true && (CHAOS_CHANCE < BLAST_RADIUS) && DELAY_RESPONSE_MS > 0) {
+      if (
+        ENSUE_CHAOS === true &&
+        CHAOS_CHANCE < BLAST_RADIUS &&
+        DELAY_RESPONSE_MS > 0
+      ) {
         setTimeout(() => {
           _end.call(res, content);
         }, DELAY_RESPONSE_MS);
       } else {
         _end.call(res, content);
       }
-      if (next) next();
     };
+    if (next) next();
   };
 };
-
 
 const chaos = modifyRes((content, req, res) => {
   try {
     const data = JSON.parse(content.toString());
-    const ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+    const ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
     REQUESTS_SO_FAR += 1;
     if (ENSUE_CHAOS === true) {
       CHAOS_CHANCE = Math.random();
       const agentData = {
         timeOfResponse: new Date().toJSON(),
-        chaosResponse: ENSUE_CHAOS,
+        chaosResponse: ENSUE_CHAOS
       };
+      //
       // testing emit method (data successfully being received by apollo.test.js & express.test.js)
-      io.emit('eucalyptus', agentData);
+      io.emit("eucalyptus", agentData);
       if (CHAOS_CHANCE < BLAST_RADIUS) {
-      // get list of keys in the data object returned by the query
+        // get list of keys in the data object returned by the query
         const dataSectionsOfResults = Object.keys(data.data);
-        dataSectionsOfResults.forEach(((name) => {
-        // if a data section is on the knock out list remove the data from the response
+        dataSectionsOfResults.forEach(name => {
+          // if a data section is on the knock out list remove the data from the response
           if (QUERY_TARGETS[name]) delete data.data[name];
-        }));
+        });
       }
     }
     return Buffer.from(JSON.stringify(data));
--- cli/commands/start.js
+++ cli/commands/start.js
@@ -1,41 +1,42 @@
-const { readFileSync } = require('fs');
-const { join } = require('path');
-const io = require('socket.io-client');
+const { readFileSync, appendFileSync } = require("fs");
+const { join } = require("path");
+const io = require("socket.io-client");
+const ALL_AGENT_DATA = [];
 
-const packageJSON = join(__dirname, '..', 'package.json');
-// **NEED TO IMPORT STOP HERE
-// const stop = require('./stop');
+const packageJSON = join(__dirname, "..", "package.json");
 
 /* The below function reads the package.json file and grabs the state
 and opens a socket.io connection to send the configure information to the agent */
 function start() {
   // console.log(state);
-  const myPackageJSON = readFileSync(
-    packageJSON,
-    'utf8',
-  );
+  const myPackageJSON = readFileSync(packageJSON, "utf8");
   const parsedMyPackageJSON = JSON.parse(myPackageJSON);
   const { state } = parsedMyPackageJSON.chaosQoala;
   // Establishes a ensueChaos property on the state that will be set to
   state.ensueChaos = true;
   console.log(state);
+  appendFileSync("results.log", JSON.stringify(state));
 
   // Retrieves the user's socket information from the package.json
   const { socketPort } = state;
   const socket = io.connect(socketPort);
-  socket.on('connect', () => {
-    socket.emit('eucalyptus', state, () => {
-      console.log('GraphQL server received config');
+  socket.on("connect", () => {
+    socket.emit("eucalyptus", state, () => {
+      console.log("GraphQL server received config");
     });
   });
 
+  socket.on("eucalyptus", agentData => {
+    ALL_AGENT_DATA.push(agentData);
+  });
+
   // The below allows the user to press any key to end the ChaosQoala
-  console.log('Press any key to exit');
+  console.log("Press any key to exit");
   // This line keeps the program from closing instantly
   process.stdin.resume();
   process.stdin.setRawMode(true);
   // Once the user has pressed the key, it should call the stop function
-  process.stdin.on('data', process.exit.bind(process, 0));
+  process.stdin.on("data", process.exit.bind(process, 0));
   function exitHandler(options, exitCode) {
     if (options.cleanup) stop();
     // if (exitCode || exitCode === 0) console.log(stop);
@@ -43,13 +44,14 @@ function start() {
   }
 
   // On exit, the exitHandler is bound to the exit key allowing stop to called
-  process.on('exit', exitHandler.bind(null, { cleanup: true }));
+  process.on("exit", exitHandler.bind(null, { cleanup: true }));
 }
 
 function stop() {
-  console.log('*********This has ended');
+  appendFileSync("results.log", JSON.stringify(ALL_AGENT_DATA));
+  console.log("*********This has ended");
 }
 
 module.exports = {
-  start,
+  start
 };
