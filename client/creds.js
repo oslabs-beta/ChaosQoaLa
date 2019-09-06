1fdc2306a5bd235a71dbb8299a9d57418939b0ed
jakeory <51100897+jakeory@users.noreply.github.com>
2019-09-06 12:14:58 -0700

    Delete creds.js

--- client/creds.js
+++
@@ -1,4 +0,0 @@
-module.exports = S3_BUCKET = "chaosqoala";
-module.exports = AWS_ACCESS_KEY_ID = "AKIAJWRNFFJVU4EFZU3A";
-module.exports = AWS_SECRET_ACCESS_KEY =
-  "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";
