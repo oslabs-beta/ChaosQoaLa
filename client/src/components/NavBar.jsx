7a139890c01a8eafb2405600aacda91731d19bfe
Jake Ory <jacobtory@yahoo.com>
2019-09-09 15:50:36 -0700

    master reconciliation

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,38 @@
-import  React, { Fragment } from 'react'
-import Upload from './Upload.jsx';
-import { NavLink } from 'react-router-dom';
+import React, { Fragment } from "react";
+import Upload from "./Upload.jsx";
+import { NavLink } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
 // import DownloadButton from './DownloadButton.jsx';
 
-function NavBar () {
-    return (
-        <div id='navbar'>
-            <img id='navBarQL'src='https://cqlimage.s3-us-west-1.amazonaws.com/AngryKoala2.png'/>
-            <NavLink id='navbarupload'to='/upload'>Upload Results </NavLink>
-            <a href='https://www.npmjs.com/'target='blank'><button id='navbardownload'>Download ChaosQoala</button></a>
-    
-        </div>
-    )
+function NavBar() {
+  return (
+    <div id="navbar">
+      <img
+        id="navBarQL"
+        src="https://cqlimage.s3-us-west-1.amazonaws.com/AngryKoala2.png"
+      />
+      <div id="rightnav">
+        <NavLink id="navbarupload" to="/upload">
+          Upload Results{" "}
+        </NavLink>
+        <a id="navbardownload" href="https://www.npmjs.com/" target="blank">
+          Download ChaosQoaLa
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
