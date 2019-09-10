08b0366ccff04014b6eb4a85a47005a4a0f3a14c
sw8wm2013 <samantha.n.wessel@gmail.com>
2019-09-09 21:19:47 -0700

    More styling added

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
             {/* <img id='mainpglogo'src='https://cqlimage.s3-us-west-1.amazonaws.com/ChaosQoaLa.png'></img> */}  
             <p id ='title'><img id='mainqoala'src='https://cqlimage.s3-us-west-1.amazonaws.com/AngryKoala2.png'></img>Chaos<span>Q</span>oa<span>L</span>a</p>
             <p id='missionstatement'>Bringing Chaos Engineering to GraphQL</p>
-            <p id='maingit'className='mainp'>Star Us on GitHub<a href='https://github.com/oslabs-beta/ChaosQoaLa'target='blank'><button id='gitbutton'><img className='star'src='https://cqlimage.s3-us-west-1.amazonaws.com/star.png'/>Star</button></a></p> 
+            <p id='starp'>Star Us on GitHub   <a href='https://github.com/oslabs-beta/ChaosQoaLa'target='blank'><button id='gitbutton'><img className='star'src='https://cqlimage.s3-us-west-1.amazonaws.com/star.png'/>Star</button></a></p> 
             <p className='mainp'>And download the ChaosQoaLa tool here:</p>
             <a href='https://www.npmjs.com/'target='blank'><button id='npmbutton'>Download</button></a>
             <p className='mainp'>Already ensuing chaos?</p>
