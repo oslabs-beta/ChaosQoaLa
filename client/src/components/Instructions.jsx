0325e67120fa0753669d4a576e5f9921766e1a20
sw8wm2013 <samantha.n.wessel@gmail.com>
2019-09-10 16:55:24 -0700

    Styling completed on Main and ThisIsUs sections

--- client/src/components/Instructions.jsx
+++ client/src/components/Instructions.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Instructions() {
     return (
         <div id='instructions'>
-            <p>Step One</p>
-            <p>Step Two</p>
-            <p>Step Three</p>
-            <p>Step Four</p>
-            <p>GIF</p>
-            <p>GIF</p>
-            <p>GIF</p>
-            <button>Download</button>
-            <Link to='/upload'>Upload Results</Link>
+            <div id='stepone'>
+                <h2>Planning Phase</h2>
+                <p>Steady state data feed and make baseline data available</p>
+                <p> Install and configure the agent on your GraphQL servers</p>
+                <p>Install the CLI on test users’ machines</p>
+            </div>
+            <div id='steptwo'>
+                <h2>Execution Phase</h2>
+                <p>Run  CLI to create a test setup</p>
+                <p>Start & stop the experiment</p>
+            </div>
+            <div id='stepthree'>
+                <h2>Upload & Analyze Results</h2>
+            </div>
+         <button id='download'>Download</button>
+         <button id='uploadlink'><NavLink to='/upload'>Upload Results</NavLink></button> 
         </div>
     )
 }
