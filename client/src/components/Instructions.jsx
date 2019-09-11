2050940bd06b2ad917763f8e68a0db945aeb4fa7
sw8wm2013 <samantha.n.wessel@gmail.com>
2019-09-10 21:15:25 -0700

    Styling on Instructions Section

--- client/src/components/Instructions.jsx
+++ client/src/components/Instructions.jsx
@@ -4,19 +4,34 @@ import { NavLink } from 'react-router-dom';
 function Instructions() {
     return (
         <div id='instructions'>
-            <div id='stepone'>
-                <h2>Planning Phase</h2>
-                <p>Steady state data feed and make baseline data available</p>
-                <p> Install and configure the agent on your GraphQL servers</p>
-                <p>Install the CLI on test users’ machines</p>
+            <div id='steponeparent'>
+                <div id='stepone'>
+                    <h2>Planning Phase</h2>
+                    <p>Steady state data feed and make baseline data available</p>
+                    <p> Install and configure the agent on your GraphQL servers</p>
+                    <p>Install the CLI on test users’ machines</p>
+                </div>
+                <div id='planningphase'>
+                    <img src='https://cqlimage.s3-us-west-1.amazonaws.com/planning-phase.png'/>
+                </div>  
             </div>
-            <div id='steptwo'>
-                <h2>Execution Phase</h2>
-                <p>Run  CLI to create a test setup</p>
-                <p>Start & stop the experiment</p>
+            <div id='steptwoparent'>
+                <div id='steptwo'>
+                    <h2>Execution Phase</h2>
+                    <p>Run  CLI to create a test setup</p>
+                     <p>Start & stop the experiment</p>
+                </div>
+                <div id='cliimage'>
+                    <img src='https://cqlimage.s3-us-west-1.amazonaws.com/CLI+Questions.png'/>
+                </div>
             </div>
-            <div id='stepthree'>
-                <h2>Upload & Analyze Results</h2>
+            <div id='stepthreeparent'>
+                <div id='stepthree'>
+                    <h2>Upload & Analyze Results</h2>
+                </div>
+                <div id='chartimage'>
+                    <img src=''/>
+                </div>
             </div>
          <button id='download'>Download</button>
          <button id='uploadlink'><NavLink to='/upload'>Upload Results</NavLink></button> 
